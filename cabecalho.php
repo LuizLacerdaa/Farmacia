0f0744f2624c9e5b8f10e605d1471b754430c965
LuizLacerdaa <luizlacerda.r@gmail.com>
2025-06-30 19:54:57 -0300

    conexao banco de dados

---
+++ cabecalho.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Farmacia Vida Saudavel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <header>
+        <img src="img/logo.png" class="logo" height="150">
+        <h1>Farmacia Vida Saudavel</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="listaprodutos.php">Produtos</a>
+        </nav>
+    </header>
